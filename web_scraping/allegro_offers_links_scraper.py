4aa2b11ca212a46400be2b0e78149c5ab955a613
Szadamek <szadam2002@gmail.com>
2023-05-12 16:11:53 +0200

    Rewrote the code to make it more readable and add moving to the next page with a random delay

--- web_scraping/allegro_offers_links_scraper.py
+++ web_scraping/allegro_offers_links_scraper.py
@@ -1,67 +1,80 @@
+import os
+import random
+import time
 from selenium import webdriver
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-
-# Ścieżka do pliku chromedriver.exe
-chrome_driver_path = 'C:/TestFiles/chromedriver.exe'
-
-# Inicjalizacja obiektu Service
-service = Service(chrome_driver_path)
-
-# Inicjalizacja sterownika Chrome z wykorzystaniem obiektu Service
-driver = webdriver.Chrome(service=service)
+from bs4 import BeautifulSoup
 
 
 def get_html_page(url):
     driver.get(url)
     html = driver.page_source
-    # with open('test.html', 'w', encoding='utf-8') as f:
-    #     f.write(html)
-
     return html
 
 
 def get_html_by_viewsource(url):
     html = get_html_page('view-source:' + url)
     soup = BeautifulSoup(html, features='html.parser')
-
-    # Extract real HTML and return it
     return soup.text
 
 
 if __name__ == '__main__':
-    url = 'https://allegro.pl/kategoria/smartfony-i-telefony-komorkowe-165'
-    html = get_html_by_viewsource(url)
+    # Set up Chrome driver
+    chrome_driver_path = 'C:/TestFiles/chromedriver.exe'
+    service = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=service)
 
-    soup = BeautifulSoup(html, 'html.parser')
-
-    # For debugging and testing selectors in Chrome
-    with open('test.html', 'w', encoding='utf-8') as f:
-        f.write(str(soup))
+    base_url = 'https://allegro.pl/kategoria/smartfony-i-telefony-komorkowe-165'
+    phones_file = 'phones.txt'
 
     # Getting phones urls from phones.txt as an array
     phones = []
-    try:
-        with open('phones.txt', 'r', encoding='utf-8') as f:
+
+    if os.path.isfile(phones_file):
+        with open(phones_file, 'r', encoding='utf-8') as f:
             for line in f:
                 phones.append(line.strip())
         print(phones)
-    except FileNotFoundError:
+    else:
         print('File not found. Creating new file.')
-        open('phones.txt', 'w', encoding='utf-8').close()
+        open(phones_file, 'w', encoding='utf-8').close()
 
-    new = 0
-    for a in soup.select(
-            '#search-results > div > div > div > div > div > div > section > article > div > div > div.mpof_ki.myre_zn.mh36_8.mjyo_6x._6a66d_5o-oq > div.m7er_k4.mj7a_4 > h2 > a'):
-        print("Found the URL:", a['href'])
-        if a['href'] not in phones:
-            phones.append(a['href'])
-            new += 1
+    total_new_phones = 0
+    page_num = 1
+    while True:
+        url = f'{base_url}?p={page_num}'
+        print(f'Strona {page_num}: {url}')
+        html = get_html_by_viewsource(url)
+        soup = BeautifulSoup(html, 'html.parser')
 
-    # Saving phones urls to phones.txt
-    with open('phones.txt', 'w', encoding='utf-8') as f:
-        for phone in phones:
-            f.write(phone + '\n')
+        # For debugging and testing selectors in Chrome
+        with open('test.html', 'w', encoding='utf-8') as f:
+            f.write(str(soup))
 
-    print(f'Added {new} new phones to phones.txt')
+        new_phones = 0
+        phone_selectors = '#search-results > div > div > div > div > div > div > section > article > div > div > div.mpof_ki.myre_zn.mh36_8.mjyo_6x._6a66d_5o-oq > div.m7er_k4.mj7a_4 > h2 > a'
+        for a in soup.select(phone_selectors):
+            print("Found the URL:", a['href'])
+            if a['href'] not in phones:
+                phones.append(a['href'])
+                new_phones += 1
+                total_new_phones += 1
+
+        if new_phones == 0:
+            # No new phones found on this page, exit the loop
+            break
+
+        # Saving phones urls to phones.txt
+        with open(phones_file, 'w', encoding='utf-8') as f:
+            for phone in phones:
+                f.write(phone + '\n')
+
+        print(f'Added {new_phones} new phones to {phones_file}')
+
+        page_num += 1
+
+        # Generate random sleep time between requests (1-3 seconds)
+        sleep_time = random.uniform(5, 10)
+        time.sleep(sleep_time)
+
+    print(f'Total {total_new_phones} new phones added')
